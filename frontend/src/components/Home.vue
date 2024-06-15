27003a1b13bd98ea81dca775728893c31785dc30
lala <lalajiang.233@gmail.com>
2024-06-15 20:59:08 +0800

    init first commit

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const clickButton= () => {
+
+}
+
+</script>
+
+<template>
+<div>
+<el-row>
+  <el-button @click="clickButton">xxxxxx</el-button>
+  <el-col span="4">
+    <div style="height: 100%;width: 100%; background-color: #535bf2">111</div>
+  </el-col>
+  <el-col span="20">
+    <div style="height: 100%;width: 100%; background-color: #213547">111</div>
+  </el-col>
+</el-row>
+</div>
+</template>
+
+<style scoped>
+
+</style>
