65b18598d1b985457008014054557b8bc82ac773
youao.du <youao.du@gmail.com>
2024-06-30 16:23:29 +0800

    [fix] change project struct.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 // 定义路由配置
 const routes = [
-    { path: '/home', component: () => import('./components/Home.vue') },
+    { path: '/', component: () => import('./pages/Home.vue') },
     // { path: '/about', component: () => import('./components/About.vue') },
 ];
 
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <H1>xxxx</H1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
